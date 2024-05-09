9b594a2e7af6b03fd88c901f2608f66d41d61bbd
dineshkota3 <dineshkota3@gmail.com>
2024-05-09 20:57:05 +0200

    Added working code for words

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import myData from './data.json';
 
 function App() {
+
+  const words = Object.keys(myData);
+  console.log(words);
+
+  const [currentCard, setCurrentCard] = useState(words[0]);
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [currentCardAnswer, setCurrentCardAnswer] = useState(myData[words[0]]);
+  
+
+  const handleNextCard = () => {
+    const rndInt = randomIntFromInterval(0, words.length-1);
+    console.log(rndInt);
+    setCurrentCard(words[rndInt]);
+    setCurrentCardAnswer(myData[words[rndInt]]);
+    setShowAnswer(false);
+  };
+
+  function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flashcards-container">
+      <div className="flashcard">
+        <div className="card-content">
+          <h2>{showAnswer ? currentCardAnswer : currentCard}</h2>
+        </div>
+        <button onClick={() => setShowAnswer(!showAnswer)}>
+          {showAnswer ? 'Show Question' : 'Show Answer'}
+        </button>
+        <button onClick={handleNextCard}>Next Card</button>
+      </div>
     </div>
   );
 }
